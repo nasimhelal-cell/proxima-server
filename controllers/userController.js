1e83889ac4b07236395a01628542485a1c74a971
Nasim <nasimhelal123>
2023-03-06 00:18:39 +0600

    Signup is completed

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,12 +2,24 @@ const User = require("../models/userModel");
 
 // user login controller
 const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  User.login(email, password);
+
   res.json({ message: "login" });
 };
 
-// user login controller
+// user singup controller
 const signupUser = async (req, res) => {
-  res.json({ message: "signup" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+
+  // res.json({ message: "signup" });
 };
 
 module.exports = {
--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -12,4 +14,36 @@ const userSchema = new Schema({
     require: true,
   },
 });
+
+//user defined method making which can be used as User.login()
+userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All fields must be filled ");
+  }
+  //check if the email is invalid
+  if (!validator.isEmail(email)) {
+    throw Error("Invalid Email ");
+  }
+  //password format -> lowercase + uppercase + number + symbol + 8+ chars
+  if (!validator.isStrongPassword(password)) {
+    throw Error(
+      "Invalid password. Your password must include a combination of uppercase and lowercase letters, numbers, and special characters. Please try again."
+    );
+  }
+  const exist = await this.findOne({ email });
+  if (exist) {
+    throw Error("Email already used");
+  }
+
+  //password encrypt or hashing
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  //create an user
+
+  const user = await this.create({ email, password: hash });
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
