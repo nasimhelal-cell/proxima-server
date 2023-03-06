00c9fc10a66dc180e7a46399a8f6c7f54bb958e7
Nasim <nasimhelal123>
2023-03-06 07:12:18 +0600

    JWT added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.SECRET, { expiresIn: "7d" });
+};
 
 // user login controller
 const loginUser = async (req, res) => {
@@ -14,7 +19,11 @@ const signupUser = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json(user);
+
+    // create token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
